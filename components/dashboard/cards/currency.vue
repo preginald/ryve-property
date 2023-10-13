f2cddf74375f7ee6456e097153703ac937072c3d
Peter Reginald <peter@reginald.au>
2023-10-13 22:33:27 +1100

    new component

---
+++ components/dashboard/cards/currency.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ title }}</h5>
+        <p class="text-xl font-normal text-gray-700 dark:text-gray-400">${{ value }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps(['title', 'value'])
+
+</script>
